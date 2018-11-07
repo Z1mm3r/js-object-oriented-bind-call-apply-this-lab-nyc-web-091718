5e11fc7b2a931c11326ee14e0b9af62046c33ea5
Z1mm3r <czimmer@ramapo.edu>
2018-11-07 13:30:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //Your code here
+
+function justInvoke(fn)
+{
+  return fn();
+}
+
+function setThisWithCall(fn,thisValue,arg)
+{
+  return fn.call(thisValue,arg)
+}
+
+function setThisWithApply(fn,thisValue,args)
+{
+  return fn.apply(thisValue,args);
+}
+
+function returnNewFunctionOf(fn,thisValue){
+  let copy = fn;
+  return copy.bind(thisValue)
+}
